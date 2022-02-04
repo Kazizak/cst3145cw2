ab7f0afa73c05eeb61f9beb6fcefc35ced1e8ceb
kazi <ka1064@live.mdx.ac.uk>
2022-02-04 15:32:58 +0000

    search 6 route corrected

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/collection/:collectionName/:searchTerm',(req,res,next)=>
 {
     //'.*'+req.params.searchTerm+'.*'
     var srch = "/"+req.params.searchTerm+"/";
-    req.collection.find({"subject": {'$in': srch}}).toArray((e,results)=>
+    req.collection.find({"subject": {'$regex': srch}}).toArray((e,results)=>
     {
         if(e) return next(e);
         res.send(results);
